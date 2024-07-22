c6c3241f62dabd1aa5b380a7e2cd681eab5af88b
bhushan-221 <bhushan.bharat@testlify.com>
2024-07-22 16:01:58 +0530

    fixed the issue

--- extract.js
+++ extract.js
@@ -3,67 +3,73 @@ const fs = require("fs");
 require("dotenv").config();
 const accessToken = process.env.ACCESS_TOKEN;
 
-let start;
-let counter = 0;
-const fileContent = fs.readFileSync("start.txt", "utf8");
-start = parseInt(fileContent);
-
-const languages = ["en_US", "de_DE", "es_ES", "fr_FR", "nl_NL"];
+// as specified in the jira
+const languages = ["de_DE", "es_ES", "fr_FR", "nl_NL"];
 
 async function getJobTitles(accessToken) {
-  const enJobTitles = await fetchJobTitles("en_US", accessToken);
-
-  for (const language of languages.slice(1)) {
-    const langJobTitles = await fetchJobTitles(language, accessToken);
-
-    for (const jobTitle of langJobTitles) {
-      const enJobTitle = enJobTitles.find(
-        (enJob) => enJob._id.$oid === jobTitle._id.$oid,
-      );
-      if (enJobTitle) {
-        const langCode = language.substring(0, 2);
-        enJobTitle.translation.push({
-          langCode,
-          translated: jobTitle.name,
-        });
-      }
-    }
-  }
-
-  fs.writeFileSync("job_titles.json", JSON.stringify(enJobTitles, null, 2));
-  console.log("Output written to job_titles.json");
-}
-
-async function fetchJobTitles(language, accessToken) {
-  const url = `https://api.linkedin.com/v2/titles?locale=${language}&start=${start}`;
+  const mainLanguage = "en_US";
+  const url = `https://api.linkedin.com/v2/titles?locale=${mainLanguage}&start=25`;
 
   try {
     const response = await axios.get(url, {
-      headers: { 
+      headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
 
-    const jobTitles = response.data.elements;
-    if (counter == 0) {
-      const lastId = jobTitles[jobTitles.length - 1].id;
-      start = start = lastId + 1;
-      console.log(`Updated start variable: ${start}`);
-      fs.writeFileSync("start.txt", `${start}\n`);
-    }
-    counter = 1;
-    return jobTitles.map((title) => {
-      const name = title.name.localized[Object.keys(title.name.localized)[0]];
+    const jobTitles = response.data.elements.map((title) => {
       return {
-        _id: { $oid: title.id },
-        name,
+        _id: title.id,
+        name: title.name.localized[Object.keys(title.name.localized)[0]],
         translation: [],
       };
     });
+
+    for (const language of languages.filter((lang) => lang !== mainLanguage)) {
+      const languageData = await fetchLanguageData(accessToken, language);
+      updateTranslations(jobTitles, languageData.data.elements, language);
+    }
+
+    const outputFile = `job_titles.json`;
+    fs.writeFileSync(outputFile, JSON.stringify(jobTitles, null, 2));
+    console.log(`Output written to ${outputFile}`);
+  } catch (error) {
+    console.error(`Error fetching job titles for ${mainLanguage}:`, error);
+  }
+}
+
+async function fetchLanguageData(accessToken, language) {
+  const url = `https://api.linkedin.com/v2/titles?locale=${language}&start=25`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return response;
   } catch (error) {
     console.error(`Error fetching job titles for ${language}:`, error);
-    return [];
   }
+}
+
+function updateTranslations(jobTitles, languageData, language) {
+  languageData.forEach((languageTitle) => {
+    const matchingTitle = jobTitles.find(
+      (title) => title._id === languageTitle.id,
+    );
+    if (matchingTitle) {
+      const langCode = language.substring(0, 2);
+      matchingTitle.translation.push({
+        langCode,
+        translated:
+          languageTitle.name.localized[
+            Object.keys(languageTitle.name.localized)[0]
+          ],
+      });
+    }
+  });
 }
 
 getJobTitles(accessToken);
