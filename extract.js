1072bc3d375f6284df9943bb8b2c7f4bb0ab95d8
bhushan-221 <bhushan.bharat@testlify.com>
2024-07-22 15:40:12 +0530

    fixed the issue

--- extract.js
+++ extract.js
@@ -39,7 +39,7 @@ async function fetchJobTitles(language, accessToken) {
 
   try {
     const response = await axios.get(url, {
-      headers: {
+      headers: { 
         Authorization: `Bearer ${accessToken}`,
       },
     });
